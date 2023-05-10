8ee60d3d30f87ad7d7062f19bda9409d12adba5f
Essa Essa <essa.essa@transferwise.com>
2023-05-10 15:12:44 +0100

    day 2

--- src/app.js
+++ src/app.js
@@ -9,6 +9,34 @@ app.get("/restaurants", async (req, res) => {
     res.json(restaurants);
 })
 
+app.get("/restaurants/:id", async (req, res) => {
+    let id = req.params;
+    let restaurant = await Restaurant.findByPk(id);
+    res.json(restaurant);
+})
+
+app.use(express.json);
+app.use(express.urlencoded);
+app.post("/restaurants", async (req, res) => {
+    const restaurant = await Restaurant.create(req.body);
+    res.json(restaurant);
+})
+
+app.put("/restaurants/:id", async (req, res) => {
+    const updated = await Restaurant.update(req.body, {where: {id: req.params.id}});
+    res.json(updated);
+})
+
+app.delete("/restaurants/:id", async (req, res) => {
+    const deleted = await Restaurant.destory({where: {id: req.params.id }});
+    res.json(deleted);
+})
+
+
+
+
+
+
 
 
 module.exports = app;
